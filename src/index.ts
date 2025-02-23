5cf0cc180c9da192e6d1436d1a5e432d8974bd8b
D-blac <a.damian.ng.od@gmail.com>
2025-02-23 07:40:45 +0100

    summarization

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config(); // Load .env synchronously
 //console.log('Loaded token:', process.env.HUGGING_FACE_TOKEN); // Debug
-//import './server'; // Start server after .env is loaded
+import './server'; // Start server after .env is loaded
